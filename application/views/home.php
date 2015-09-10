35af839e28eeb21523c200a5799e536c7fb423fe
suesm27 <suesm27@gmail.com>
2015-09-10 11:56:33 -0700

    added bouncy pomelo png

--- application/views/home.php
+++ application/views/home.php
@@ -4,8 +4,22 @@
 	<title>Friendly Pomelo</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+ <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+ <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+ <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+ <script type="text/javascript">
+ $(document).ready(function() {
+   $(function(){
+      //Add bounce effect on Click of the DIV
+      $('#logo').click(function () {
+        $(this).effect("bounce", { times:5 }, 2000);
+      });
+    });
+ });
+ </script>
  <style></style>
  <link rel="stylesheet" type="text/css" href="/assets/style.css">
 </head>
@@ -13,7 +27,7 @@
   <div class="main-container">
     <div class="container">
       <div id="logo">
-        <img src="/assets/images/pomelo.jpg">
+        <img src="/assets/images/pomelo.png">
       </div>
     </div> <!-- /container -->
   </div>
