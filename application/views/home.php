5b57ec337ec01698836be6e3b64486a6dc97eddc
suesm27 <suesm27@gmail.com>
2015-09-10 14:58:13 -0700

    removed background from swinging pomelo

--- application/views/home.php
+++ application/views/home.php
@@ -4,13 +4,11 @@
 	<title>Friendly Pomelo</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
- <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
  <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
  <script src="/assets/jquery.ui.touch-punch.min.js"></script>
-
  <script type="text/javascript">
  $(document).ready(function() {
    $(function(){
@@ -23,7 +21,6 @@
     });
  });
  </script>
- <style></style>
  <link rel="stylesheet" type="text/css" href="/assets/style.css">
 </head>
 <body>
