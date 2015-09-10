08534e649ea188f22cacb02cad233910344cf197
suesm27 <suesm27@gmail.com>
2015-09-10 16:39:02 -0700

    changed color for pomelos

--- application/views/home.php
+++ application/views/home.php
@@ -28,26 +28,26 @@
     <div class="container">
       <div id="bouncable">
         <h3>Click me!</h3>
-        <img class="upright" src="/assets/images/pomelo.png">
+        <img class="upright" src="/assets/images/pomelo-red.png">
       </div>
       <div class="spacer">
       </div>
-      <div id="draggable" class="ui-widget-content" style="border: none; width: 192px">
+      <div id="draggable" class="ui-widget-content">
         <div id="animated-example" class="animated swing">
-        <img class="rotate45" src="/assets/images/pomelo.png">
+        <img class="rotate45" src="/assets/images/pomelo-orange.png">
         <h3>Drag me!</h3>
       </div>
     </div>
       <div class="spacer">
       </div>
       <div id="animated-example3" class="animated3 shake">
-          <img class="upright" style="width: 93px" src="/assets/images/pomelo.png">
-          <img class="upright" style="width: 93px" src="/assets/images/pomelo.png">
+          <img class="upright" style="width: 93px" src="/assets/images/pomelo-purple.png">
+          <img class="upright" style="width: 93px" src="/assets/images/pomelo-purple.png">
       </div>
       <div class="spacer">
       </div>
       <div id="animated-example4" class="animated4 hinge">
-         <img class="rotate105" src="/assets/images/pomelo.png">
+         <img class="rotate105" src="/assets/images/pomelo-green.png">
       </div>
     </div> <!-- /container -->
   </div>
